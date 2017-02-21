052e3b5b1c446a10f8fd9e266ff7c2e20abbc63f
joudaon <jonuda77@gmail.com>
2017-02-21 16:26:02 +0100

    Locating an element by Name tag

---
+++ src/tutorialselenium/IdNameDemo.java
@@ -0,0 +1,19 @@
+package tutorialselenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class IdNameDemo {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		String baseURL = "http://www.google.com";
+		driver.manage().window().maximize();  //Maximizes the windows
+		driver.get(baseURL);
+		driver.findElement(By.id("lst-ib")).sendKeys("letskodeit"); //Finds that element ID and writes 'letskodeit' in the textarea
+		driver.findElement(By.name("btnG")).click(); //Locating button by name and clicking it
+	}
+
+}
