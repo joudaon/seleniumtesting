b2a8e006d7be90e6ca3da4bc9d5505e43e2de2e2
joudaon <jonuda77@gmail.com>
2017-02-22 09:22:49 +0100

    Locating elements by LinkText and partialLinkText

---
+++ src/tutorialselenium/FindByLinkText.java
@@ -0,0 +1,21 @@
+package tutorialselenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FindByLinkText {
+
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new FirefoxDriver();
+		String baseURL = "https://letskodeit.teachable.com/p/practice";
+		driver.manage().window().maximize();  //Maximizes the windows
+		driver.get(baseURL);
+
+		driver.findElement(By.linkText("Login")).click();		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.partialLinkText("Sign")).click();
+	}
+
+}
