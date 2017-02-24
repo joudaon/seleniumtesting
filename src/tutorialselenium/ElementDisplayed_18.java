d63e08b7e3e59fb8b58ee3a805ce862b3cf10b3f
joudaon <jonuda77@gmail.com>
2017-02-24 09:23:16 +0100

    Changed "ElementDiplayed" filename

--- src/tutorialselenium/ElementDisplayed_18.java
+++ src/tutorialselenium/ElementDisplayed_18.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ElementDisplayed {
+public class ElementDisplayed_18 {
 
 	private WebDriver driver;
 	String baseUrl1;
