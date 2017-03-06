4ee57ed09de81867271d3cadfdf1271c81664822
joudaon <jonuda77@gmail.com>
2017-03-06 14:12:23 +0100

    Data Driven Testing - Writing Data to Excel File

--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -3,5 +3,5 @@ package utilities;
 public class Constants {
       public static final String URL = "http://letskodeit.com";
       public static final String File_Path = "C:\\Users\\Uda\\Desktop\\";
-      public static final String File_Name = "ExcelData.xlsx";
+      public static final String File_Name = "ExcelDataSingleDataSet.xlsx";
 }
