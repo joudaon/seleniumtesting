d7e204679a40dd1d1e4f81f0d43b77fbec307c79
joudaon <jonuda77@gmail.com>
2017-03-06 16:02:46 +0100

    Name changed to Profile Demo file

--- src/tutorialselenium/ProfilesDemo_35.java
+++ src/tutorialselenium/ProfilesDemo_35.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
 
-public class ProfilesDemo {
+public class ProfilesDemo_35 {
 
 	public static void main(String[] args) {
 		String baseURL = "http://www.google.com";
